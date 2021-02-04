4746d4992b843826d675d9d9993db92411bbfb0e
Lily Hughes-Robinson <oaguy1@gmail.com>
2021-02-03 19:25:50 -0500

    Add upvote_count to ListItemSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -22,9 +22,15 @@ class WishlistSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ListItemSerializer(serializers.HyperlinkedModelSerializer):
+    upvote_count = serializers.SerializerMethodField()
+
+    def get_upvote_count(self, obj):
+        return Upvote.objects.filter(list_item=obj.id).count()
+
+    
     class Meta:
         model = ListItem
-        fields = ['url', 'name', 'description', 'wishlist', 'created_by', 'created_at', 'updated_at']
+        fields = ['url', 'name', 'description', 'wishlist', 'created_by', 'created_at', 'updated_at', 'upvote_count']
 
 
 class WishlistDetailSerializer(serializers.HyperlinkedModelSerializer):
